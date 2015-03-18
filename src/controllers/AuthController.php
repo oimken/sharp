cb3f72491eaeff5939788b9bf3b1c1c2ff5d912c
oimken <omken@gmail.com>
2015-03-18 16:42:56 +0800

    Fixed the redirect after loging
    Check referer, if it is login page then redirect to "admin/"; otherwise
    just go back;

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
-class AuthController extends Controller {
+class AuthController extends Controller
+{
 
     /**
      * @return \Illuminate\View\View
@@ -33,22 +34,23 @@ class AuthController extends Controller {
 
             // Next: check credentials
             $authService = SharpSiteConfig::getAuthService();
-            if($user = $authService->login($data["login"], $data["password"]))
-            {
+            if ($user = $authService->login($data["login"], $data["password"])) {
                 // Login succeed
                 Session::put("sharp_user", $user);
-                return redirect()->intended("admin/cms");
-            }
-            else
-            {
+
+                // Check referer, if it is login page then redirect to "admin/"; otherwise just go back;
+                if (preg_match("#.*log2in[/]?$#", $request->server('HTTP_REFERER'))) {
+                    return redirect()->intended("admin");
+                } else {
+                    return Redirect::back();
+                }
+
+            } else {
                 return redirect()->back()
                     ->withInput()
                     ->with(["flashMessage" => trans('sharp::messages.login_invalid')]);
             }
-        }
-
-        catch(ValidationException $e)
-        {
+        } catch (ValidationException $e) {
             return redirect()->back()
                 ->withInput()
                 ->withErrors($e->getErrors());
